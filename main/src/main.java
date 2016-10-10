6b15b2e0433cbc94a6ef6711b3d7960824b605e0
jangelsb <jangelsb.vcs@gmail.com>
2016-10-09 18:31:10 -0700

    * Subtitle.java     - Implemented findFirstNormalSub and findFirstForeignLangOnlySub!     - Added a verbose option, it's on by default * new jar file * New main showing off new features! You can even do chaining (builder style!) e.g.,     - new Subtitle(video, Subtitle.Type.ForeignLangOnly).setVerbose(false).download();

--- main/src/main.java
+++ main/src/main.java
@@ -4,8 +4,9 @@ import java.io.File;
 
 public class main {
     public static void main(String args[]) {
-        File video = new File("./The.Big.Bang.Theory.S08E22.720p.HDTV.X264-DIMENSION.mkv");
-        Subtitle sub = new Subtitle(video, Subtitle.Type.HI);
+        File video = new File("./Whiskey.Tango.Foxtrot.2016.720p.BluRay.x264-DRONES.mkv");
+        Subtitle sub = new Subtitle(video, Subtitle.Type.ForeignLangOnly);
+        sub.setVerbose(false);
         sub.download();
     }
 }
